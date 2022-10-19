fe38adf0c0a5e131d943abf7f7bad8eb4b577a71
shurarama <altchist@yandex.ru>
2022-10-19 21:35:08 +0300

    added termios chenge

--- termsetup.c
+++ termsetup.c
@@ -19,3 +19,21 @@ void    signal_ctlc_heredoc(int sig)
                 write(STDERR_FILENO, "\n", 1);
         }
 }
+
+int	termios_change(bool echo_ctl_chr)
+{
+	struct termios	terminos_p;
+	int				status;
+
+	status = tcgetattr(STDOUT_FILENO, &terminos_p);
+	if (status == -1)
+		return (ERROR);
+	if (echo_ctl_chr)
+		terminos_p.c_lflag |= ECHOCTL;
+	else
+		terminos_p.c_lflag &= ~(ECHOCTL);
+	status = tcsetattr(STDOUT_FILENO, TCSANOW, &terminos_p);
+	if (status == -1)
+		return (ERROR);
+	return (0);
+}
